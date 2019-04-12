e52a0593b69b93149dd24acba4391ceb80db7b77
samiahmad13 <samiahmadcr7@outlook.comn>
2019-04-12 13:06:24 -0500

    d

--- src/Core/Ticket.java
+++ src/Core/Ticket.java
@@ -1,17 +1,28 @@
 public class Ticket {
     private int value;
-    private City a;
-    private City b;
+    private String a;
+    private String b;
     private boolean finished;
-    public Ticket(City a, City b, int value) {
+    public Ticket(String a, String b, int value) {
         this.a = a;
         this.b = b;
         this.value = value;
+        this.finished = false;
     }
-
+    public String getA(){
+        return a;
+    }
+    public String getB(){
+        return b;
+    }
+    public int getValue(){
+        return value;
+    }
     public boolean getFinished() {
         return finished;
     }
-
+    public String toString(){
+        return a+" "+b+" "+value;
+    }
 
 }
