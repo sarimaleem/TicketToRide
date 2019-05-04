0b4ee17e274edab6d9019eba93ed4f7fa125b83e
Sarim Aleem <sarimaleem99@gmail.com>
2019-05-03 21:49:52 -0500

    only a small part of the endgame

--- src/Graphics/GraphicsBoard.java
+++ src/Graphics/GraphicsBoard.java
@@ -242,12 +242,23 @@ public class GraphicsBoard extends JPanel implements MouseListener {
     }
 
     public void drawEndGame(Graphics2D graphics2D) {
-        int width = 1000, height = 600, x = 100, y = 100;
+        int width = 1250, height = 600, x = 100, y = 100;
         graphics2D.setColor(new Color(114, 0, 255));
         graphics2D.setStroke(new BasicStroke(3));
         graphics2D.fillRect(x, y, width, height);
         graphics2D.setColor(Color.black);
         graphics2D.drawRect(x, y, width, height);
+
+        graphics2D.setColor(Color.black);
+        graphics2D.setFont(new Font("serif", Font.BOLD, 60));
+        graphics2D.drawString("The End!", 600, 150);
+
+        
+
+
+
+
+
     }
 
     public void mousePressed(MouseEvent e) {
